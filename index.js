26c02a4cad09b70835ac134d3261e9020c1aa6ef
Rafael0Motta <rafaelm.programador@gmail.com>
2024-05-21 16:36:37 -0300

    Criação da section home e sua responsividade

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+/* SCROLL BUTTON */
+window.addEventListener("scroll", function () {
+  var topBtn = document.getElementById("topBtn");
+  if (window.scrollY > 100) {
+    topBtn.classList.add("active");
+  } else {
+    topBtn.classList.remove("active");
+  }
+});
 
 /* HAMBURGUER MENU */
 function showSidebar() {
-    const sidebar = document.querySelector(".navItensContainerMobile");
-    sidebar.style.display = "flex";
-  }
-  
-  function hideSidebar() {
-    const sidebar = document.querySelector(".navItensContainerMobile");
-    sidebar.style.display = "none";
-  }
-  
+  const sidebar = document.querySelector(".navItensContainerMobile");
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".navItensContainerMobile");
+  sidebar.style.display = "none";
+}
