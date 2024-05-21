774e8e4d34e83db5aaa24a0d1753ce377c0c2162
Rafael0Motta <rafaelm.programador@gmail.com>
2024-05-21 10:52:26 -0300

    Criação do menu de navegação, bem como a responsividade do mesmo. Ainda será preciso atualizar as cores posteriormente.

---
+++ index.js
@@ -0,0 +1,12 @@
+
+/* HAMBURGUER MENU */
+function showSidebar() {
+    const sidebar = document.querySelector(".navItensContainerMobile");
+    sidebar.style.display = "flex";
+  }
+  
+  function hideSidebar() {
+    const sidebar = document.querySelector(".navItensContainerMobile");
+    sidebar.style.display = "none";
+  }
+  
